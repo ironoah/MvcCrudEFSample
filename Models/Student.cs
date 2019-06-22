d861c853084fbd14f59f821543b4c4e308460719
natani <ironoah@gmail.com>
2019-06-23 05:02:10 +0900

    Update Student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,17 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
-    {
-        public int ID { get; set; }
-        public string LastName { get; set; }
-        public string FirstMidName { get; set; }
-        public DateTime EnrollmentDate { get; set; }
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
 
-        public ICollection<Enrollment> Enrollments { get; set; }
+    public partial class Student
+    {
+        public Student()
+        {
+            this.Enrollments = new HashSet<Enrollment>();
+        }
+        [Key]
+        public int ID { get; set; }
+        [DisplayName("苗字")]
+        [StringLength(20)]
+        public string LastName { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [MaxLength(10, ErrorMessage = "BloggerName must be 10 characters or less"), MinLength(5)]
+        public string FirstMidName { get; set; }
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+        [StringLength(50)]
+        public string MiddleName { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        [Timestamp]
+        public Byte[] TimeStamp { get; set; }
     }
 }

b156b29c2b9a9f42aced1d1a52e779dc4ec2b0d5
natani <ironoah@gmail.com>
2019-06-23 05:04:13 +0900

    Update SchoolInitializer.cs

--- DAL/SchoolInitializer.cs
+++ DAL/SchoolInitializer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -41,18 +41,18 @@ namespace ContosoUniversity.DAL
             context.SaveChanges();
             var enrollments = new List<Enrollment>
             {
-            new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
-            new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
-            new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
-            new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
-            new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
-            new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
+            new Enrollment{StudentID=1,CourseID=1050,Grade=(decimal?)Grade.A},
+            new Enrollment{StudentID=1,CourseID=4022,Grade=(decimal?)Grade.C},
+            new Enrollment{StudentID=1,CourseID=4041,Grade=(decimal?)Grade.B},
+            new Enrollment{StudentID=2,CourseID=1045,Grade=(decimal?)Grade.B},
+            new Enrollment{StudentID=2,CourseID=3141,Grade=(decimal?)Grade.F},
+            new Enrollment{StudentID=2,CourseID=2021,Grade=(decimal?)Grade.F},
             new Enrollment{StudentID=3,CourseID=1050},
             new Enrollment{StudentID=4,CourseID=1050,},
-            new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
-            new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
+            new Enrollment{StudentID=4,CourseID=4022,Grade=(decimal?)Grade.F},
+            new Enrollment{StudentID=5,CourseID=4041,Grade=(decimal?)Grade.C},
             new Enrollment{StudentID=6,CourseID=1045},
-            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
+            new Enrollment{StudentID=7,CourseID=3141,Grade=(decimal?)Grade.A},
             };
             enrollments.ForEach(s => context.Enrollments.Add(s));
             context.SaveChanges();
